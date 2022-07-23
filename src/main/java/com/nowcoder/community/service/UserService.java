566d080c63470aeee5e052291f1d6ed8ba08978f
wangzhuang <2268903339@qq.com>
2022-07-23 23:33:04 +0800

    第五次提交

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -187,5 +187,9 @@ public class UserService implements CommunityConstant {
         return userMapper.updateHeader(userId, headerUrl);
     }
 
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
+    }
+
 
 }
